95a63a87aba2ede1b8b0a6f41f448119c1e69122
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-02-06 14:53:51 -0600

    Added comments to each answer.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -58,8 +58,9 @@ for (let i=0; i<stars.length; i++){
   console.log(stars[i])
 }
 //END OF LOOP
-
-// 3. While loops
+//"i" is 0, because that is the number assigned to the first element in an array.
+//"i<stars.length" as the 'to' part of the iteration will ensure we loop through all elements in the array.
+//"i++" as the 'do' part of the iteration moves the loop through each element.
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
@@ -70,6 +71,10 @@ while (i<stars.length){
   i++
 }
 //END OF LOOP
+//"i" is set to '0' for the first element of the array.
+//"(i<stars.length)"" as the condition will make sure we loop through the entire array.
+//"console.log(stars[i])" in the statement will console.log the star located in the current loop's element.
+//"i++" in the statement will increase the value of "i" by 1 after each console.log.
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
@@ -79,6 +84,11 @@ while (count<6){
   count++
 }
 //END OF LOOP
+//We create a variable named "count" and make it's initial value '0'.
+//"(count<6)" is our condition because we want the loop to stop at '5'.
+//"console.log(count)" in our statement will log it's value.
+//"count++" will increase the value of "count" by 1 each time the loop is run.
+
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
@@ -88,3 +98,7 @@ while (digits>4){
   digits--
 }
 //END OF LOOP
+//We create a variable named "digits" and set it's initial value at '10' because that is where our countdown will start.
+//"(digits>4)" is our condition because we want the countdown to stop at 5.
+//"console.log(digits)" in the statement will log the current value of "digits".
+//"digits--" will decrease the value of "digits" by 1 each time through the loop.
